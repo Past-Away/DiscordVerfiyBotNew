c442d0a80a3209b1b0b5f72c7104b56f4523f440
T0myyde <lpmcbagi@gmail.com>
2021-10-26 20:10:02 +0200

    Initial commit

---
+++ src/main/java/de/tom/discordverifybot/Discordverifybot.java
@@ -0,0 +1,17 @@
+package de.tom.discordverifybot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Discordverifybot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
